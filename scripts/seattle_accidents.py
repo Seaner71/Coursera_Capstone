89f76f1b9835eedf6a7ab7bc35c2d7b2a481da77
echo 'hello world <ssmyth1071@gmail.com>
2020-11-19 19:29:09 -0500

    add csv file and update py file

--- scripts/seattle_accidents.py
+++ scripts/seattle_accidents.py
@@ -1,6 +1,112 @@
 # This file contains the code for the Coursera Capstone IBM Data Science Certificate
-
-import pandas as pd
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import confusion_matrix, accuracy_score, r2_score, classification_report
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
 
-print('Hello Capstone Project Course!')
+
+df = pd.read_csv('Data-Collisions.csv')
+df.info()
+
+df = df.drop(['OBJECTID','INCKEY','LOCATION','COLDETKEY','REPORTNO','STATUS','INTKEY','EXCEPTRSNCODE',
+              'EXCEPTRSNDESC','SEVERITYDESC','INCDATE','SDOT_COLCODE','SDOT_COLDESC','SDOTCOLNUM','ST_COLCODE',
+              'ST_COLDESC','SEGLANEKEY','CROSSWALKKEY','INCDTTM'],axis=1)
+def missing_function(df):
+    missing_data = df.isnull()
+    missing_data.head()
+
+    for column in missing_data.columns.values.tolist():
+        print(column)
+        print(missing_data[column].value_counts())
+        print(df[column].value_counts())
+        print("")
+
+missing_function(df)
+df.replace("Unknown", np.nan, inplace = True)
+df.replace("Other", np.nan, inplace = True)
+
+#removing columns with more than 20% values missing (INATTENTIONIND,PEDROWNOTGRNT,SPEEDING)
+df = df.drop(["INATTENTIONIND","PEDROWNOTGRNT","SPEEDING"],axis=1)
+
+#removing rows for columns with less than 20% values missing (X, Y,COLLISIONTYPE,JUNCTIONTYPE,
+                                                            #UNDERINFL,WEATHER,ROADCOND,LIGHTCOND)
+df.dropna(subset=["X","Y","COLLISIONTYPE","JUNCTIONTYPE","UNDERINFL","WEATHER","ROADCOND","LIGHTCOND"],
+          axis=0, inplace=True)
+
+#making sure all missing values are handled with
+print(df.info())
+missing_function(df)
+
+df= df.sample(frac=1,random_state=0,replace=False)
+
+# 1 - Put all severity code 2 class in a separate dataset.
+df_scode2 = df.loc[df['SEVERITYCODE'] == 2]
+
+# 2 - Randomly select 58188 observations from the severity code 1(majority class)
+df_scode1 = df.loc[df['SEVERITYCODE'] == 1].sample(n=48926,random_state=42)
+
+# 3 - concatenating datasets to get balanced dataset
+df_balanced = pd.concat([df_scode1,df_scode2])
+df_balanced = df_balanced.sample(frac=1,random_state=0,replace=False)
+
+#Replacing 0 with N and 1 with Y as this column has mixed datatype values, making it consistent
+df_balanced['UNDERINFL'] = df_balanced['UNDERINFL'].replace(['0'],'N')
+df_balanced['UNDERINFL'] = df_balanced['UNDERINFL'].replace(['1'],'Y')
+
+#checking if dataset balanced
+print(df_balanced.info())
+print(df_balanced['SEVERITYCODE'].value_counts())
+
+X = df_balanced.iloc[:,1:]
+
+#Encoding Categorical Features - Training Dataset
+X = pd.get_dummies(data=X, columns=['ADDRTYPE','COLLISIONTYPE','JUNCTIONTYPE','WEATHER',
+                                                            'ROADCOND','LIGHTCOND','UNDERINFL','HITPARKEDCAR'])
+
+Y = df_balanced[['SEVERITYCODE']]
+print(X.info())
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y,test_size=0.2,random_state=0)
+
+#Scaling training data as per the requirements
+scaler = StandardScaler()
+X_train[['X','Y','PERSONCOUNT','PEDCOUNT','PEDCYLCOUNT','VEHCOUNT']] = scaler.fit_transform(
+    X_train[['X','Y','PERSONCOUNT','PEDCOUNT','PEDCYLCOUNT','VEHCOUNT']])
+
+#Scaling test data as per the requirements
+scaler = StandardScaler()
+X_test[['X','Y','PERSONCOUNT','PEDCOUNT','PEDCYLCOUNT','VEHCOUNT']] = scaler.fit_transform(
+    X_test[['X','Y','PERSONCOUNT','PEDCOUNT','PEDCYLCOUNT','VEHCOUNT']])
+
+X_train.info()
+
+missing_function(X_train)
+print(X_train.info())
+print(X_test.info())
+
+#Fitting and Predictions
+lr = LogisticRegression(random_state = 0)
+lr.fit(X_train,Y_train)
+lr_predictions = lr.predict(X_test)
+
+#Confusion Matrix
+lr_cm = confusion_matrix(Y_test,lr_predictions)
+print(lr_cm,'\n')
+
+#Classification Report
+lr_cr = classification_report(Y_test,lr_predictions)
+print(lr_cr,'\n')
+
+#Accuracy
+acc = accuracy_score(Y_test,lr_predictions)
+print(acc,'\n')
+accDict = {}
+accDict['LR'] = acc
